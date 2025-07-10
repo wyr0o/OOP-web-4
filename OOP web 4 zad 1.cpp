25976d8a3774fd414a0ad2e8960828f372b209fe
wyr0o <brykin-2006@mail.ru>
2025-07-10 20:21:20 +0300

    Add files via upload

---
+++ OOP web 4 zad 1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Shape
+{
+protected:
+	int sides{};
+	std::string name{};
+public:
+	int getSides(){return sides;}
+
+	std::string getName() { return name; }
+
+	Shape(int SetSides, std::string SetName) : sides{SetSides}, name {SetName}
+	{}
+};
+class triangle : public Shape
+{
+public:
+	triangle() : Shape{ 3, "Треугольник" }
+	{}
+};
+class quadrangle : public Shape
+{
+public:
+	quadrangle() : Shape{4, "Четырёхугольник"}
+	{}
+};
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	Shape shape(0, "фигура: ");
+	triangle tri;
+	quadrangle quad;
+
+	std::cout << "Количество сторон" << std::endl;
+	std::cout << shape.getName() << ": " << shape.getSides() << std::endl;
+	std::cout << tri.getName() << ": " << tri.getSides() << std::endl;
+	std::cout << quad.getName() << ": " << quad.getSides() << std::endl;
+
+
+
+}
